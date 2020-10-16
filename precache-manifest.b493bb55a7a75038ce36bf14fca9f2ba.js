81c91c9758bf208d361d17823a98d0d2ca59d5f2
mstefaniak <marcin.stefaniak@gmail.com>
2020-10-16 08:42:24 +0000

    Deploying to gh-pages from  @ 8ba3191a25c7132ecde4a2a8ba0fee1acc988320 🚀

---
+++ precache-manifest.b493bb55a7a75038ce36bf14fca9f2ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa77b7eff757f2ff8fc3308e569759a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c418d46746180bee2ad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b973137d9f055f3cd25a",
+    "url": "/static/js/2.36a440aa.chunk.js"
+  },
+  {
+    "revision": "f6bb6e7e06135325042ea16538284523",
+    "url": "/static/js/2.36a440aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c418d46746180bee2ad",
+    "url": "/static/js/main.9db28c47.chunk.js"
+  },
+  {
+    "revision": "1c13a92c99224a1db1e9",
+    "url": "/static/js/runtime-main.bd4a7b6e.js"
+  }
+]);
