0e8f82ff2849c24d79e075a8261c38ed27f9296f
mstefaniak <marcin.stefaniak@gmail.com>
2020-10-16 11:34:38 +0000

    Deploying to gh-pages from  @ e261309223d4d1194fc38bbc6f32ecc03123c466 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42ea501dd4ee98a9f4efa0e31a51bd9a.js"
+  "/precache-manifest.4562ff73a0130f44180394568d30a5be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
