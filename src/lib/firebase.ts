5179555de6962340e21ebc956466098a95c02e33
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2020-10-16 13:31:42 +0200

    set firebase prod config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,20 @@ import Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+const isDev = process.env.NODE_ENV === "development";
+const projectId = isDev
+  ? "book-a-table-96a18"
+  : process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const apiKey = isDev
+  ? "AIzaSyBCooZY6_EXqK5bDrW-toY0IGJ_xM7B8bE"
+  : process.env.REACT_APP_FIREBASE_API_KEY;
+
 const config = {
-  apiKey: "AIzaSyBCooZY6_EXqK5bDrW-toY0IGJ_xM7B8bE",
-  authDomain: "book-a-table-96a18.firebaseapp.com",
-  databaseURL: "https://book-a-table-96a18.firebaseio.com",
-  projectId: "book-a-table-96a18",
-  storageBucket: "book-a-table-96a18.appspot.com",
+  apiKey,
+  authDomain: `${projectId}.firebaseapp.com`,
+  databaseURL: `https://${projectId}.firebaseio.com`,
+  projectId,
+  storageBucket: `${projectId}.appspot.com`,
   messagingSenderId: "173934858452",
   appId: "1:173934858452:web:50fdcc7645b3d229572b41",
 };
